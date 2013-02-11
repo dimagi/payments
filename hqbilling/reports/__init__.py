05e26ee5d6a326dbaf4006841f27bb5626324d8b
Biyeun Buczyk <biyeun@dimagi.com>
2013-02-11 11:43:39 -0500

    deleted old chunk of useless code

--- hqbilling/reports/__init__.py
+++ hqbilling/reports/__init__.py
@@ -23,37 +23,3 @@ class BaseBillingAdminInterface(BaseCRUDAdminInterface, HQBillingReport):
     def validate_document_class(self):
         # todo implement properly
         return True
-
-
-#class UpdatableItem(GenericTabularReport, HQBillingReport):
-#    report_template_path = "hqbilling/reports/updatable_item_report.html"
-#    fields = []
-#    hide_filters = True
-#    exportable = False
-#
-#    form_class = SMSRateForm
-#    item_class = SMSRate
-#
-#    @property
-#    def report_context(self):
-#        context = super(UpdatableItem, self).report_context
-#        context.update(
-#            form=dict(
-#                name=self.form_class.__name__,
-#                item=self.item_class.__name__
-#            ),
-#            status_message=self.status_message
-#        )
-#        return context
-#
-#    @property
-#    def rows(self):
-#        all_rates = self.item_class.view(self.item_class.couch_view(),
-#            reduce=False,
-#            include_docs=True
-#        ).all()
-#        return [rate.as_row for rate in all_rates]
-#
-#    @property
-#    def status_message(self):
-#        return "Currency is in <strong>$ USD</strong>."
