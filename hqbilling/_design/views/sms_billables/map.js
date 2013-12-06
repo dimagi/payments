a35f1d123c36068e786f99b42a410f184c305d68
Biyeun Buczyk <biyeun@dimagi.com>
2013-12-06 16:10:37 -0500

    adding additional information in emit of sms_billables view

--- hqbilling/_design/views/sms_billables/map.js
+++ hqbilling/_design/views/sms_billables/map.js
@@ -6,12 +6,12 @@ function (doc) {
 
         emit(["domain", doc.domain, doc.billable_date], 1);
         emit(["type domain", doc.doc_type, doc.domain, doc.billable_date], 1);
-        emit(["domain log_id", doc.domain, doc.log_id, doc.billable_date], 1);
+        emit(["domain log_id", doc.domain, doc.log_id, doc.billable_date, doc.modified_date], 1);
 
         emit(["domain direction", doc.domain, doc.direction, doc.billable_date], 1);
         emit(["type domain direction", doc.doc_type, doc.domain, doc.direction, doc.billable_date], 1);
 
         emit(["billable", doc.domain], 1);
-        emit(["billable date", doc.billable_date, doc.domain], 1);
+        emit(["billable type date", doc.doc_type, doc.billable_date], 1);
     }
 }
