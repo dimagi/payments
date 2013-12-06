a800bb08f0c9fda80d3b2f796885e3ebc9465d43
Biyeun Buczyk <biyeun@dimagi.com>
2013-12-06 15:58:17 -0500

    update sms_billables view for extra information necessary to fix MachBillables bug

--- hqbilling/_design/views/sms_billables/map.js
+++ hqbilling/_design/views/sms_billables/map.js
@@ -6,10 +6,12 @@ function (doc) {
 
         emit(["domain", doc.domain, doc.billable_date], 1);
         emit(["type domain", doc.doc_type, doc.domain, doc.billable_date], 1);
+        emit(["domain log_id", doc.domain, doc.log_id, doc.billable_date], 1);
 
         emit(["domain direction", doc.domain, doc.direction, doc.billable_date], 1);
         emit(["type domain direction", doc.doc_type, doc.domain, doc.direction, doc.billable_date], 1);
 
         emit(["billable", doc.domain], 1);
+        emit(["billable date", doc.billable_date, doc.domain], 1);
     }
 }
